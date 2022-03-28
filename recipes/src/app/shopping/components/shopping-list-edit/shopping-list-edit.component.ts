39d551b3f8be1247095bce4643c076ee3d046149
ihorpidhornyi <ihorp@rabota.ua>
2022-03-28 12:37:44 +0300

    feat(shopping): added validator for amount

--- recipes/src/app/shopping/components/shopping-list-edit/shopping-list-edit.component.ts
+++ recipes/src/app/shopping/components/shopping-list-edit/shopping-list-edit.component.ts
@@ -10,9 +10,11 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ShoppingListEditComponent implements OnInit {
+  private positiveNumbersRegExp = new RegExp(/^[1-9]+[0-9]*$/)
+
   public form = new FormGroup({
     name: new FormControl('', Validators.required),
-    amount: new FormControl(null, Validators.required),
+    amount: new FormControl(null, [Validators.required, Validators.pattern(this.positiveNumbersRegExp)])
   });
 
   constructor(private shoppingService: ShoppingService) {}
