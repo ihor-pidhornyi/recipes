893f9f8f6ae380b7e8954118f50e8c3a42d64a82
ihorpidhornyi <ihorp@rabota.ua>
2022-04-04 13:58:07 +0300

    feat(shared): added header animation

--- recipes/src/app/shared/components/header/header.component.ts
+++ recipes/src/app/shared/components/header/header.component.ts
@@ -9,12 +9,45 @@ import { BehaviorSubject, Observable, Subject } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { MatSlideToggleChange } from '@angular/material/slide-toggle';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import {
+  animate,
+  sequence,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [
+    trigger('openClose', [
+      state(
+        'open',
+        style({
+          opacity: '1',
+          display: 'block'
+        })
+      ),
+      state(
+        'closed',
+        style({
+          display: 'none',
+          opacity: '0',
+        })
+      ),
+      transition('open => closed', [sequence([
+        animate('300ms ease-in-out'),
+      ])]),
+      transition('closed => open', [sequence([
+        style({ display: 'block' }),
+        animate('300ms ease-in-out')
+      ])]),
+    ]),
+  ],
 })
 export class HeaderComponent implements OnInit {
   @Input() darkMode: boolean | undefined;
